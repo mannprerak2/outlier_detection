593f4d66436cedb5da9d661db9a1ec559b272e19
Prerak Mann <mannprerak2@gmail.com>
2021-04-24 20:50:42 +0530

    Basic randomised outlier detector

--- distance.py
+++ distance.py
@@ -4,14 +4,14 @@ Utility to provde distance between points.
 import Levenshtein
 
 class Distance:
-    def __init__(self, sampleNumbers):
-        self.sampleNumbers = sampleNumbers
+    def __init__(self, data):
+        self.data = data
     def distance(self, i, j):
         # TODO: add caching.
 
         # (Levenshtein.distance isn't recognized for some reason) so we add this -
         # pylint: disable=no-member
-        return Levenshtein.distance(self.sampleNumbers[i], self.sampleNumbers[j])
+        return Levenshtein.distance(self.data[i], self.data[j])
 
     def closest(self, i, arr) -> int:
         distances = [self.distance(i,x) for x in arr]
--- main.py
+++ main.py
@@ -5,16 +5,19 @@ hierarchical clustering algorithm.
 
 import random
 import distance
-from statistics import mean
+from statistics import mean, stdev
 
-class KNN:
+
+class KNNValues:
     '''
     Stores the k nearest neighbours of a data item.
     '''
-    def __init__(self, kNN):
+
+    def __init__(self, kNN, distances):
         self.kNN = kNN
         self.__average = None
-        self.distances = []
+        self.distances = distances
+        self.distances.sort()
 
     def getMax(self):
         return self.distances[-1]
@@ -22,7 +25,7 @@ class KNN:
     def setMax(self, value):
         self.distances[-1] = value
         self.__average = None
-        sorted(self.distances)
+        self.distances.sort()
 
     def average(self):
         if self.__average is None:
@@ -35,10 +38,10 @@ class Node:
     A Node is a cluster.
     '''
 
-    def __init__(self, samples = []):
+    def __init__(self, sampleIndexes=[]):
         # Indices of all samples under this cluster.
         self.sampleNumbers = []
-        self.sampleNumbers.extend(samples)
+        self.sampleNumbers.extend(sampleIndexes)
 
         # Randomly chosen cluster centers.
         self.centroidSeeds = []
@@ -47,17 +50,15 @@ class Node:
         self.sampleNumbers.append(sample)
 
     def DHCA(self, dist_knn, edge_knn, kNN, nodeArray,
-             currentNode, k, data, maxClusterSize, threshold,
+             k, maxClusterSize, threshold,
              distance: distance.Distance) -> None:
         '''
         Input:
         - dist_knn: sorted distances of kNN for each data item.
-        - edge_knn: average of distances of kNN for eacch data item.
+        - edge_knn: db outlier scores
         - kNN: number of kNNs for a data item.
         - nodeArray: Array of Nodes(clusters).
-        - currentNode: current Node of the Node array.
         - k: number of clusters at each step.
-        - data: input data size.
         - maxClusterSize: max size allowed for a cluster.
         - threshold: The value used to filter.
 
@@ -70,7 +71,7 @@ class Node:
         self.centroidSeeds = random.sample(population=self.sampleNumbers, k=k)
 
         # Generate k new nodes and map them with the index.
-        nodeMap= {}
+        nodeMap = {}
         for center in self.centroidSeeds:
             nodeMap[center] = Node([center])
 
@@ -79,22 +80,107 @@ class Node:
                 continue
             # Loop over nodes that are not center.
 
-            # Get nearest center
+            # Get nearest center.
             j = distance.closest(i, self.centroidSeeds)
 
             if dist_knn[i].getMax() > distance.distance(i, j):
                 dist_knn[i].setMax(distance.distance(i, j))
+                # Using average distance as value of db outlier scores.
+                edge_knn[i] = dist_knn[i].average()
 
             if dist_knn[i].average() > threshold:
                 nodeMap[j].addSample(i)
 
-        for k, v in nodeMap:
+        for v in nodeMap.values():
             if len(v.sampleNumbers) > maxClusterSize:
                 nodeArray.append(v)
 
 
+def readHumanGeneData():
+    '''
+    Reads the gene sequences from the file
+    and stores as a dictionary
+    '''
+    f = open('data/human_gene.data', "r")
+    data = {}
+    label = ''
+    genes = ''
+    count = 0
+    if f.mode == 'r':
+        fl = f.readlines()
+        for x in fl:
+            # print(x)
+            if x[0] == '>':
+                count = count + 1
+                if label != '':
+                    data[label] = genes
+                    genes = ''
+                label = x[4:].rstrip()
+            else:
+                genes = genes + x.rstrip()
+        data[label] = genes
+    return data
+
+
+def getThreshold(edge_knn):
+    return mean(edge_knn)+stdev(edge_knn)
+
+
 def main():
-    pass
+    mp = readHumanGeneData()
+
+    labels = list(mp.keys())
+    data = list(mp.values())
+    size = len(labels)
+
+    sampleIndexes = list(range(0, size))
+
+    kNN = 3
+    k = 3
+    maxClusterSize = 4
+
+    dist_knn = []
+    edge_knn = [0]*size
+
+    distanceFinder = distance.Distance(data)
+
+    # Fill sequential dist_knn values for initial upper bound.
+    for i in range(0, size-kNN):
+        distances = []
+        for j in range(0, kNN):
+            distances.append(distanceFinder.distance(i, i+j+1))
+        dist_knn.append(KNNValues(kNN, distances))
+
+    for i in range(size-kNN, size):
+        distances = []
+        for j in range(0, kNN):
+            distances.append(distanceFinder.distance(i-j-1, i))
+        dist_knn.append(KNNValues(kNN, distances))
+
+    # Set edge_knn (db outlier score).
+    for i in range(0, len(edge_knn)):
+        edge_knn[i] = dist_knn[i].average()
+
+    # Reverse Sorted list of indexes with key=edge_knn value.
+    db_outlier_indexes = list(range(0, size))
+    db_outlier_indexes.sort(reverse=True, key=lambda x: edge_knn[x])
+
+    # Number of outliers to find.
+    n = 3
+
+    for i in range(5):
+        print(db_outlier_indexes[:n])
+        # Created with single cluster of all elements.
+        nodeArray = [Node(sampleIndexes=sampleIndexes)]
+        threshold = getThreshold(edge_knn)
+        for node in nodeArray:
+            node.DHCA(dist_knn=dist_knn, edge_knn=edge_knn,
+                    kNN=kNN, nodeArray=nodeArray, k=k, maxClusterSize=maxClusterSize,
+                    distance=distanceFinder, threshold=threshold)
+
+        # Sort db outliers
+        db_outlier_indexes.sort(reverse=True, key=lambda x: edge_knn[x])
+
 
 
 if __name__ == '__main__':
